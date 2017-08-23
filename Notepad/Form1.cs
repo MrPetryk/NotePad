091d187077fc88e75857c79b8761365d75117be9
Petro <pikhotapetro@gmail.com>
2017-08-24 00:01:11 +0200

    File/New

--- Notepad/Form1.cs
+++ Notepad/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Notepad
 {
@@ -20,6 +21,29 @@ namespace Notepad
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (textBox1.TextLength != 0)
+            {
+               DialogResult res= MessageBox.Show("Notepad has some symbols. Save it?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (res == DialogResult.Yes)
+                {
+                    SaveFileDialog sfd = new SaveFileDialog();
+                    sfd.FileName = "NoName";
+                    sfd.Filter = "Text Files | *.txt";
+                    sfd.DefaultExt = "txt";
+
+                    if (sfd.ShowDialog() == DialogResult.OK)
+                    {                 
+                        File.WriteAllText(sfd.FileName, textBox1.Text);
+                    }
+                }
+                
+                    textBox1.Clear();
+                
+            }
         }
     }
 }
